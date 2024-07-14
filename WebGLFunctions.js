3237e167351b17ba31216243be6c012237059b29
pKilba <72137640+pKilba@users.noreply.github.com>
2024-07-15 00:04:51 +0300

    Create WebGLFunctions.js

---
+++ WebGLFunctions.js
@@ -0,0 +1,7 @@
+function ShowWallet() {
+    document.getElementById('connect-container').style.display = 'block';
+}
+
+function HideWallet() {
+    document.getElementById('connect-container').style.display = 'none';
+}
