6bc773488cb1997b98a3d4818591feacfe6aac4f
pKilba <72137640+pKilba@users.noreply.github.com>
2024-07-22 12:33:22 +0300

    Delete Build directory

--- Build/StreamingAssets/WebGLFunctions.js
+++
@@ -1,8 +0,0 @@
-mergeInto(LibraryManager.library, {
-    ShowWallet: function() {
-        document.getElementById('connect-container').style.display = 'block';
-    },
-    HideWallet: function() {
-        document.getElementById('connect-container').style.display = 'none';
-    }
-});
